cc70631503f5193842b3b0e1bc40d220b4d31347
TutoHacks <50992017+TutoHacks@users.noreply.github.com>
2021-05-26 11:24:48 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -3,4 +3,34 @@
 --https://github.com/Whit3XLightning--
 --------------------------------------
 
-RegisterCommand(Config.commandName, function(source, args, rawCommand) TriggerClientEvent("wld:delallveh", -1) end, Config.restricCommand)
+local ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj  end)
+
+ESX.RegisterServerCallback('tutohacks:welcheRolle', function(source, cb)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    cb(xPlayer.getGroup())
+end)
+
+function sendToDiscord(title, message, footer)
+	local embed = {
+		  {
+			  ["color"] = "#f50014",
+			  ["title"] = "**".. title .."**",
+			  ["description"] = message,
+			  ["footer"] = {
+			  ["text"] = footer,
+			  },
+		  }
+	  }  
+	PerformHttpRequest(Config.Webhook, function(err, text, headers) end, 'POST', json.encode({username = "Discord Logs", embeds = embed}), { ['Content-Type'] = 'application/json' })
+end
+
+RegisterServerEvent("wld:sendtoDiscord")
+AddEventHandler("wld:sendtoDiscord", function(titleembed, messagembed, footerembed)
+    sendToDiscord(titleembed, messagembed, footerembed)
+end)
+
+--------------------------------------
+-------Customized by TutoHacks--------
+-----https://github.com/TutoHacks-----
+--------------------------------------
